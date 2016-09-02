c546dae6bfb7e9a7e018591de91bb71a76c4c01f
Stewart Knapman <info@stewartknapman.com>
2016-09-03 09:53:54 +1200

    hero tweaks

--- _src/js/core.js
+++ _src/js/core.js
@@ -2,15 +2,19 @@
 var supports = !!document.querySelector && !!window.addEventListener;
 if (!supports) return;
 
+var _ = require('./_lib.js');
 var Load = require('./_load.js');
 var load = new Load();
 
 load.css(['/c/b.css']);
 load.svg(['/i/i.svg']);
-load.img({
-  ig: '<svg class="icon icon-github"><use xlink:href="#icon-github"></use></svg>',
-  it: '<svg class="icon icon-twitter"><use xlink:href="#icon-twitter"></use></svg>',
-  id: '<svg class="icon icon-dribbble"><use xlink:href="#icon-dribbble"></use></svg>'
+_.ready(function () {
+  load.img({
+    ig: '<svg class="icon icon-github"><use xlink:href="#icon-github"></use></svg>',
+    it: '<svg class="icon icon-twitter"><use xlink:href="#icon-twitter"></use></svg>',
+    id: '<svg class="icon icon-dribbble"><use xlink:href="#icon-dribbble"></use></svg>',
+    p: '<img src="http://placehold.it/1440x600">'
+  });
 });
 
 // load GA
