b02bed8732e092c0c3f5bc1a2658677a911bfc0f
Stewart Knapman <info@stewartknapman.com>
2016-09-17 12:58:28 +1200

    product

--- _src/js/core.js
+++ _src/js/core.js
@@ -7,6 +7,7 @@ var _ = require('./_lib.js');
 var Load = require('./_load.js');
 var load = new Load();
 
+/*
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('/sw.js').then(function(reg) {
     console.log('Yey!', reg);
@@ -14,6 +15,7 @@ if ('serviceWorker' in navigator) {
     console.log('Boo!', err);
   });
 }
+*/
 
 // If css.supports load extra css
 // add classes for clip-path and shape
@@ -28,19 +30,29 @@ var supportsCSS = function (property, className) {
 if (CSS && CSS.supports) {
   supportsCSS('(shape-outside: polygon(0 0, 100% 0, 100% 100%, 0 80%)) and (-webkit-shape-outside: polygon(0 0, 100% 0, 100% 100%, 0 80%)) and ((clip-path: polygon(0 0, 100% 0, 100% 100%, 0 80%)) or (-webkit-clip-path: polygon(0 0, 100% 0, 100% 100%, 0 80%)))', 'css-shape');
 }
-
 load.css(['/c/b.css']);
-load.svg(['/i/i.svg']);
 
+// Load SVG files
+var svgFiles = ['/i/i.svg'];
+if (document.querySelector('body').className === 'ix') {
+  svgFiles.push('/i/poster.svg');
+}
+load.svg(svgFiles);
+
+// On DOMContentLoaded start adding images, svg, and other assets
 _.ready(function () {
   load.replace({
     l: '<svg class="logo"><use xlink:href="#logo"></use></svg>',
     ig: '<svg class="icon icon-github"><use xlink:href="#icon-github"></use></svg>',
     it: '<svg class="icon icon-twitter"><use xlink:href="#icon-twitter"></use></svg>',
     id: '<svg class="icon icon-dribbble"><use xlink:href="#icon-dribbble"></use></svg>',
+    posi: '<svg class="product-thumb"><use xlink:href="#poster"></use></svg>',
     p: '<img class="hero-img" src="/i/peckhams_2048.jpg">',
-    phd: '<img class="hero-img" src="/i/phd_2048.jpg">'
+    phd: '<img class="hero-img" src="/i/phd_2048.jpg">',
+    jtms: '<div data-embed_type="product" data-shop="luciddesign.myshopify.com" data-product_name="&#39;Jack of all Trades; Master of Some&#39; Poster" data-product_handle="jack-of-all-trades-master-of-some-poster" data-has_image="false" data-display_size="compact" data-redirect_to="checkout" data-buy_button_text="Buy the poster" data-buy_button_out_of_stock_text="Out of Stock" data-buy_button_product_unavailable_text="Unavailable" data-button_background_color="ffffff" data-button_text_color="03b9e3" data-product_modal="false" data-product_title_color="000000" data-next_page_button_text="Next page"></div>'
   });
+  
+//   load.js(['https://widgets.shopifyapps.com/assets/widgets/embed/client.js']);
 });
 
 // load GA
@@ -56,6 +68,7 @@ _gaq.push(['_trackPageview']);
 */
 
 // load typekit
+/*
 (function(d) {
   var config = {
     kitId: 'bmn1olh',
@@ -64,3 +77,4 @@ _gaq.push(['_trackPageview']);
   },
   h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
 })(document);
+*/
