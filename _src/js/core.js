9439736e74868243c5cd4d9c5c6a5a902a20f282
Stewart Knapman <info@stewartknapman.com>
2016-09-17 13:08:47 +1200

    no shopify fallback

--- _src/js/core.js
+++ _src/js/core.js
@@ -49,10 +49,10 @@ _.ready(function () {
     posi: '<svg class="product-thumb"><use xlink:href="#poster"></use></svg>',
     p: '<img class="hero-img" src="/i/peckhams_2048.jpg">',
     phd: '<img class="hero-img" src="/i/phd_2048.jpg">',
-    jtms: '<div data-embed_type="product" data-shop="luciddesign.myshopify.com" data-product_name="&#39;Jack of all Trades; Master of Some&#39; Poster" data-product_handle="jack-of-all-trades-master-of-some-poster" data-has_image="false" data-display_size="compact" data-redirect_to="checkout" data-buy_button_text="Buy the poster" data-buy_button_out_of_stock_text="Out of Stock" data-buy_button_product_unavailable_text="Unavailable" data-button_background_color="ffffff" data-button_text_color="03b9e3" data-product_modal="false" data-product_title_color="000000" data-next_page_button_text="Next page"></div>'
+    jtms: '<div data-embed_type="product" data-shop="luciddesign.myshopify.com" data-product_name="&#39;Jack of all Trades; Master of Some&#39; Poster" data-product_handle="jack-of-all-trades-master-of-some-poster" data-has_image="false" data-display_size="compact" data-redirect_to="checkout" data-buy_button_text="Buy the poster" data-buy_button_out_of_stock_text="Out of Stock" data-buy_button_product_unavailable_text="Unavailable" data-button_background_color="ffffff" data-button_text_color="03b9e3" data-product_modal="false" data-product_title_color="000000" data-next_page_button_text="Next page"><a class="b" href="https://luciddesign.myshopify.com/cart/25377140360:1" target="_blank">Buy the poster</a></div>'
   });
   
-//   load.js(['https://widgets.shopifyapps.com/assets/widgets/embed/client.js']);
+  load.js(['https://widgets.shopifyapps.com/assets/widgets/embed/client.js']);
 });
 
 // load GA
@@ -68,7 +68,6 @@ _gaq.push(['_trackPageview']);
 */
 
 // load typekit
-/*
 (function(d) {
   var config = {
     kitId: 'bmn1olh',
@@ -77,4 +76,3 @@ _gaq.push(['_trackPageview']);
   },
   h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
 })(document);
-*/
