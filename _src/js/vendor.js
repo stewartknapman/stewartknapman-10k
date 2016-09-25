f1f96e035b93bced0328bb7000b15522b85ee05e
Stewart Knapman <info@stewartknapman.com>
2016-09-25 13:51:06 +1300

    clean up

--- _src/js/vendor.js
+++ _src/js/vendor.js
@@ -1,4 +1,3 @@
-/*
 // Load GA
 var _gaq = _gaq || [];
 _gaq.push(['_setAccount', 'UA-11095759-1']);
@@ -8,7 +7,6 @@ _gaq.push(['_trackPageview']);
   ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
   var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
 })();
-*/
 
 // Load Typekit
 (function(d) {
